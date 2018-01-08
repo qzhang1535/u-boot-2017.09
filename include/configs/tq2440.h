2d7a549225a02961bf453438a7fc444f5e35681f
jonne <jonnezhang@126.com>
2018-01-08 16:56:45 +0800

    modify bootargs and nand partitions for lede

--- include/configs/tq2440.h
+++ include/configs/tq2440.h
@@ -2,7 +2,6 @@
 #define _TQ2440_H
 
 
-#define CONFIG_XF2440_SERIAL	1
 #define CONFIG_BAUDRATE			115200
 #define CONFIG_NR_DRAM_BANKS	1
 
@@ -67,26 +66,9 @@
 /*mtd*/
 #define CONFIG_MTD_DEVICE						1
 #define MTDIDS_DEFAULT 							"nand0=nand0"
-#define MTDPARTS_DEFAULT 						"mtdparts=nand0:0x100000@0x0(uboot),0x100000@0x100000(env),0x100000@0x200000(dtb),0x500000@0x300000(kernel),0x4000000@0x800000(rootfs),-(reserved)"
+#define MTDPARTS_DEFAULT 						"mtdparts=nand0:0x200000@0x0(uboot),0x600000@0x200000(kernel),0x4000000@0x800000(rootfs),-(reserved)"
 #define MTD_ACTIVE_PART 						"nand0,2"
 
-/*
-#define CONFIG_DRIVER_DM9000					1
-#ifdef CONFIG_DRIVER_DM9000
-	#define CONFIG_DM9000_NO_SROM				1
-	#define CONFIG_DM9000_BASE				0x20000000
-	#define DM9000_IO							CONFIG_DM9000_BASE
-	#define DM9000_DATA						(CONFIG_DM9000_BASE + 4)
-	#define CONFIG_DM9000_USE_16BIT 			1
-#endif
-
-
-#define CONFIG_ETHADDR		10:1a:1b:1c:1d:1e
-#define CONFIG_NETMASK        255.255.255.0
-#define CONFIG_IPADDR		192.168.1.230
-#define CONFIG_SERVERIP		192.168.1.100
-#define CONFIG_GATEWAYIP	192.168.1.1
-*/
 
 #define CONFIG_SETUP_MEMORY_TAGS		1
 #define CONFIG_CMDLINE_TAG				1
